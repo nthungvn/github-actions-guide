208d54504d65bb34fde1d22397d6758aa4094642
Hung Nguyen <nthung.vlvn@gmail.com>
2022-05-24 07:28:45 +0000

    bundle/build action to one file

--- .github/actions/hello-world-javascript-action/hello-world-java-script-action.sh
+++ .github/actions/hello-world-javascript-action/hello-world-java-script-action.sh
@@ -17,3 +17,18 @@ docker run --rm -it \
   -w /action \
   node:16-alpine \
   npm install @actions/core @actions/github
+
+# Bundle/build into one file
+docker run --rm -it \
+  -v $HOME/.npm:/root/.npm \
+  -v `pwd`/.github/actions/hello-world-javascript-action:/action \
+  -w /action \
+  node:16-alpine \
+  npm install --save-dev @vercel/ncc
+
+docker run --rm -it \
+  -v $HOME/.npm:/root/.npm \
+  -v `pwd`/.github/actions/hello-world-javascript-action:/action \
+  -w /action \
+  node:16-alpine \
+  npm run bundle
