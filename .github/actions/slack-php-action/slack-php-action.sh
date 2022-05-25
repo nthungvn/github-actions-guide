dd37120ed5145812e3e34216f7b83abaa3fa0616
Hung Nguyen <nthung.vlvn@gmail.com>
2022-05-25 09:45:23 +0000

    feat: add the action to send slack message

---
+++ .github/actions/slack-php-action/slack-php-action.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Install the packages
+docker run --rm --interactive --tty \
+  --volume $PWD/.github/actions/slack-php-action:/app \
+  --volume ${COMPOSER_HOME:-$HOME/.composer}:/tmp \
+  composer require rmccue/requests
+
+
+# Test send the message
+# Find the token here: https://api.slack.com/apps/A03GFJKHU58/install-on-team?
+docker run --rm --interactive --tty \
+  --volume $PWD/.github/actions/slack-php-action:/app \
+  --volume ${COMPOSER_HOME:-$HOME/.composer}:/tmp \
+  -e SLACK_TOKEN=FIND_TOKEN \
+  php:alpine3.14 php /app/script.php
+
+
+# Use block kit builder to custom message
+# - https://app.slack.com/block-kit-builder
+
+# Test build docker
+docker build -t slack-php-action:latest .
+
+docker-compose -f slack-php-action-compose.yml up
+
+docker-compose -f slack-php-action-compose.yml up --build
