0e47f21c131d34c1119f60b62fdd4778e486afda
Hung Nguyen <nthung.vlvn@gmail.com>
2022-05-25 04:00:53 +0000

    refactor: using github command

--- .github/actions/hello-world-docker-action/entrypoint.sh
+++ .github/actions/hello-world-docker-action/entrypoint.sh
@@ -1,5 +1,20 @@
 #!/bin/sh -l
 
+echo "::debug::Debug Message"
+echo "::warning ::Debug Message"
+echo "::error ::Debug Message"
+
+echo "::add-mask::$INPUT_NAME"
+
 echo "Hello $1"
 time=$(date)
 echo "::set-output name=time::$time"
+
+echo "::group::Some expandable logs"
+echo "Some stuff";
+echo "Some stuff";
+echo "Some stuff";
+echo "Some stuff";
+echo "::endgroup::"
+
+echo "INPUT_NAME=$1" >> $GITHUB_ENV
